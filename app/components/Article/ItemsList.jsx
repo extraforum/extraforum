d90b25a54bdf4127fd57c29c781964992dbdcca9
Dang Minh Duc <112186500+ducware@users.noreply.github.com>
2023-09-05 19:24:06 +0700

    Update ItemsList.jsx

--- app/components/Article/ItemsList.jsx
+++ app/components/Article/ItemsList.jsx
@@ -73,6 +73,130 @@ function ItemsList() {
           }
         />
       </ListItem>
+      <ListItem alignItems="flex-start">
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src="https://th.bing.com/th/id/R.e15bb9256f3d0b6cecc5bbd8e13ba3e8?rik=VECMHACyZippLw&pid=ImgRaw&r=0" />
+        </ListItemAvatar>
+        <ListItemText
+          primary="Hỏi đáp"
+          secondary={
+            <React.Fragment>
+              <Typography
+                sx={{ display: 'inline' }}
+                component="span"
+                variant="body2"
+                color="text.primary"
+              >
+              </Typography>
+              {"Nơi mọi người cùng nhau chia sẻ khó khăn, cùng nhau giải quyết"}
+            </React.Fragment>
+          }
+        />
+      </ListItem>
+      <Divider variant="inset" component="li" />
+      <ListItem alignItems="flex-start">
+        <ListItemAvatar>
+          <Avatar alt="Travis Howard" src="https://pbs.twimg.com/profile_images/1122184484923662336/N9YFx3wD_400x400.png" />
+        </ListItemAvatar>
+        <ListItemText
+          primary="Javascript"
+          secondary={
+            <React.Fragment>
+              <Typography
+                sx={{ display: 'inline' }}
+                component="span"
+                variant="body2"
+                color="text.primary"
+              >
+              </Typography>
+              {"Nơi hỏi đáp mọi chủ đề về Javacsript (Scope, context, bất đồng bộ, promise ...)"}
+            </React.Fragment>
+          }
+        />
+      </ListItem>
+      <Divider variant="inset" component="li" />
+      <ListItem alignItems="flex-start">
+        <ListItemAvatar>
+          <Avatar alt="Cindy Baker" src="https://th.bing.com/th/id/R.083d315e12252c3d2ca31d31f6315365?rik=6bMIx9VtcN9TCQ&pid=ImgRaw&r=0" />
+        </ListItemAvatar>
+        <ListItemText
+          primary="NodeJS"
+          secondary={
+            <React.Fragment>
+              <Typography
+                sx={{ display: 'inline' }}
+                component="span"
+                variant="body2"
+                color="text.primary"
+              >
+              </Typography>
+              {'Chia sẻ, thảo luận về Node.JS , Javascript'}
+            </React.Fragment>
+          }
+        />
+      </ListItem>
+      <ListItem alignItems="flex-start">
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src="https://th.bing.com/th/id/R.e15bb9256f3d0b6cecc5bbd8e13ba3e8?rik=VECMHACyZippLw&pid=ImgRaw&r=0" />
+        </ListItemAvatar>
+        <ListItemText
+          primary="Hỏi đáp"
+          secondary={
+            <React.Fragment>
+              <Typography
+                sx={{ display: 'inline' }}
+                component="span"
+                variant="body2"
+                color="text.primary"
+              >
+              </Typography>
+              {"Nơi mọi người cùng nhau chia sẻ khó khăn, cùng nhau giải quyết"}
+            </React.Fragment>
+          }
+        />
+      </ListItem>
+      <Divider variant="inset" component="li" />
+      <ListItem alignItems="flex-start">
+        <ListItemAvatar>
+          <Avatar alt="Travis Howard" src="https://pbs.twimg.com/profile_images/1122184484923662336/N9YFx3wD_400x400.png" />
+        </ListItemAvatar>
+        <ListItemText
+          primary="Javascript"
+          secondary={
+            <React.Fragment>
+              <Typography
+                sx={{ display: 'inline' }}
+                component="span"
+                variant="body2"
+                color="text.primary"
+              >
+              </Typography>
+              {"Nơi hỏi đáp mọi chủ đề về Javacsript (Scope, context, bất đồng bộ, promise ...)"}
+            </React.Fragment>
+          }
+        />
+      </ListItem>
+      <Divider variant="inset" component="li" />
+      <ListItem alignItems="flex-start">
+        <ListItemAvatar>
+          <Avatar alt="Cindy Baker" src="https://th.bing.com/th/id/R.083d315e12252c3d2ca31d31f6315365?rik=6bMIx9VtcN9TCQ&pid=ImgRaw&r=0" />
+        </ListItemAvatar>
+        <ListItemText
+          primary="NodeJS"
+          secondary={
+            <React.Fragment>
+              <Typography
+                sx={{ display: 'inline' }}
+                component="span"
+                variant="body2"
+                color="text.primary"
+              >
+              </Typography>
+              {'Chia sẻ, thảo luận về Node.JS , Javascript'}
+            </React.Fragment>
+          }
+        />
+      </ListItem>
     </List>
   );
 }
